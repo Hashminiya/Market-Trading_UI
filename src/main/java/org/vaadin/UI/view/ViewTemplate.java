2ec32d17e8e11ff290d41434c2b9e41844319d5b
Yagilbr <yagilbr@bgu.post.ac.il>
2024-06-02 15:15:35 +0300

    add logo to ViewTemplate, remove GreetService.java, remove MainView.java remove AppShell.java

--- src/main/java/org/vaadin/UI/view/ViewTemplate.java
+++ src/main/java/org/vaadin/UI/view/ViewTemplate.java
@@ -8,14 +8,17 @@ import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route("")
+
 public abstract class ViewTemplate extends VerticalLayout {
     public ViewTemplate() {
         HorizontalLayout header = new HorizontalLayout();
-        Button logo = new Button("Logo");
+
+        Button logoButton = createLogoButton();
+
         Button loginButton = new Button("Login");
         Button signUpButton = new Button("Sign Up");
         Button logoutButton = new Button("Logout");
@@ -23,12 +26,37 @@ public abstract class ViewTemplate extends VerticalLayout {
         Button createNewStoreButton = new Button("Create New Store");
         Button systemManagerButton = new Button("System Manager");
 
-        header.add(logo, loginButton, signUpButton, logoutButton, manageStoresButton, createNewStoreButton, systemManagerButton);
+        header.add(logoButton, loginButton, signUpButton, logoutButton, manageStoresButton, createNewStoreButton, systemManagerButton);
         header.setWidth("100%");
+        header.setHeight("60px");
         header.setSpacing(true);
         header.setAlignItems(Alignment.CENTER);
 
         add(header);
+    }
+
+    private Button createLogoButton(){
+        Image logoImage = new Image("icons/logoFinal.png","");
+        logoImage.setWidth("100px");
+        logoImage.setHeight("60px");
+        Button logoButton = new Button(logoImage);
+        logoButton.setHeight("60px");
+        logoButton.setWidth("100px");
+
+        logoButton.addClickListener(event -> {
+            // Navigate to the homepage
+            getUI().ifPresent(ui -> ui.navigate(""));
+        });
+        return logoButton;
+    }
+
+    private Button createManageStoresButton(){
+        Button manageStoreButton = new Button("Manage Stores");
+        manageStoreButton.addClickListener(event -> {
+            // Navigate to the homepage
+            getUI().ifPresent(ui -> ui.navigate("settings/inventory"));
+        });
+        return  manageStoreButton;
     }
     public abstract void setUp();
 }
