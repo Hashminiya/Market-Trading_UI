db1c3f8cecfd69383d805c6bd6623ed406cfc637
noamgilad <noamgilad98@gmail.com>
2024-06-14 15:09:47 +0300

    add store DTO

---
+++ src/main/java/org/vaadin/UI/model/DTOs/StoreDTO.java
@@ -0,0 +1,42 @@
+package org.vaadin.UI.model.DTOs;
+import java.io.Serializable;
+import java.util.List;
+
+public class StoreDTO implements Serializable {
+    private String name;
+    private String description;
+    private List<ItemDTO> items;
+
+
+    public StoreDTO(String name, String description, List<ItemDTO> items) {
+        this.name = name;
+        this.description = description;
+        this.items = items;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<ItemDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemDTO> items) {
+        this.items = items;
+    }
+
+}
