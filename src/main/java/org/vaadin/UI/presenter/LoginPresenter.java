1cb8d68e035a3031d88e0cf039118ad3449d2d70
Noam Shurki <shurkin@post.bgu.ac.il>
2024-06-15 18:38:22 +0300

    add loginModel class

--- src/main/java/org/vaadin/UI/presenter/LoginPresenter.java
+++ src/main/java/org/vaadin/UI/presenter/LoginPresenter.java
@@ -1,57 +1,25 @@
 package org.vaadin.UI.presenter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import org.vaadin.UI.Util.Credentials;
+import org.vaadin.UI.model.models.LoginModel;
 import org.vaadin.UI.presenter.Interfaces.IPresenter;
 import org.vaadin.UI.view.ViewInterface.ILoginView;
-import org.springframework.stereotype.Component;
 
-@Component
 public class LoginPresenter implements IPresenter {
-    private final ILoginView view;
-    private final RestTemplate restTemplate;
 
-    @Autowired
+    private final ILoginView view;
+    private final LoginModel model;
+
     public LoginPresenter(ILoginView view) {
-        this.restTemplate = new RestTemplate();
         this.view = view;
+        this.model = new LoginModel();
     }
 
     public void onLogin() {
-
-        // Define the URL
-        String url = "http://localhost:8080/user/login";
-
-        // Define the query parameters
-        String username = view.getUsername();
-        String password = view.getPassword();
-
-        // Create the request body as a query string
-        String requestBody = "userName=" + username + "&password=" + password;
-
-        // Set the headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        // Create an HttpEntity with the request body and headers
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
-        try {
-            ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
-
-            if (response.getStatusCode() == HttpStatus.OK) {
-                // Login successful
-                Credentials.setToken(response.getBody(),this);
-                view.showNotification("Login successful for user: " + username);
-            } else {
-                // Login failed
-                view.showNotification("Login failed. User is already exists.");
-            }
-        } catch (Exception e) {
-            // Request failed
-            view.showNotification("Couldn't connect to server. Please try again later.");
-        }
+        String result = model.login(view.getUsername(), view.getPassword());
+        view.showNotification(result);
     }
 
     @Override
