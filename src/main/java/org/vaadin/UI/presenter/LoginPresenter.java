1d0ec975c8e8e9ac4209aa89958b0fe86d325f54
idonag <idonag@post.bgu.ac.il>
2024-06-16 12:28:11 +0300

    login

--- src/main/java/org/vaadin/UI/presenter/LoginPresenter.java
+++ src/main/java/org/vaadin/UI/presenter/LoginPresenter.java
@@ -3,6 +3,7 @@ package org.vaadin.UI.presenter;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import org.vaadin.UI.Util.Credentials;
+import org.vaadin.UI.Util.SuccessCallBack;
 import org.vaadin.UI.model.models.LoginModel;
 import org.vaadin.UI.presenter.Interfaces.IPresenter;
 import org.vaadin.UI.view.ViewInterface.ILoginView;
@@ -17,8 +18,8 @@ public class LoginPresenter implements IPresenter {
         this.model = new LoginModel();
     }
 
-    public void onLogin() {
-        String result = model.login(view.getUsername(), view.getPassword());
+    public void onLogin(SuccessCallBack callBack) {
+        String result = model.login(view.getUsername(), view.getPassword(), callBack);
         view.showNotification(result);
     }
 
