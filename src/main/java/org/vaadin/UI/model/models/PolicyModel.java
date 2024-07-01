7291c037a1c4a0bbbd9b2036130033da8380594b
idonag <idonag@post.bgu.ac.il>
2024-07-01 21:31:44 +0300

    presenting policy to user

--- src/main/java/org/vaadin/UI/model/models/PolicyModel.java
+++ src/main/java/org/vaadin/UI/model/models/PolicyModel.java
@@ -55,27 +55,4 @@ public class PolicyModel {
             return null;
         }
     }
-    public String assignManager(String token, String userName, String storeNumber, Set<String> permissions) {
-        String url = "http://localhost:8080/storeManagement/assignStoreManager";
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("token", token);
-        params.add("storeId", storeNumber);
-        params.add("newManagerI", userName);
-
-        // Convert set of permissions to list
-        List<String> permissionsList = new ArrayList<>(permissions);
-        params.put("permissions", permissionsList);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
-
-        try {
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-            return response.getBody();
-        } catch (Exception e) {
-            return e.getMessage();
-        }
-    }
 }
